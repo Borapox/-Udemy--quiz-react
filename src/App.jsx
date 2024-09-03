bbd3f18091a9ad7bc18821ecbbf6e13ba2aed95f
Borapox <muller.mdi@gmail.com>
2024-09-03 20:30:46 -0300

    criação do context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Welcome from "./components/Welcome";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500">
+      
+      <Welcome />
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { QuizProvider } from './context/Quiz.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </StrictMode>,
 )
